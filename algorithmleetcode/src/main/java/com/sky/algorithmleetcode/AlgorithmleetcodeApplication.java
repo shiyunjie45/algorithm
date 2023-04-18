bed408299a2f1332b4d8f834c2cdd44d609b0e5e
shiyunjie <jiejie12530@163.com>
2023-04-18 14:26:18 +0800

    初始化leetcode算法题

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/AlgorithmleetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.sky.algorithmleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmleetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmleetcodeApplication.class, args);
+    }
+
+}
---
+++ algorithmleetcode/src/test/java/com/sky/algorithmleetcode/AlgorithmleetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.algorithmleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmleetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
