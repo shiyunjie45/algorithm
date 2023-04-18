63116764eb70d90eba20f6de6994820dbe70ef18
shiyunjie <jiejie12530@163.com>
2023-04-18 16:15:53 +0800

    编写了leetCode的1和2的算法,并编写了测试用例

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L1Solution.java
@@ -0,0 +1,73 @@
+package com.sky.algorithmleetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * 提示：
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+ * Related Topics
+ * 数组
+ * 哈希表
+ */
+class L1Solution {
+    public static int[] twoSum1(int[] nums,int target){
+    for (int i = 0; i < nums.length; i++) {
+            for (int i1 = i+1; i1 < nums.length; i1++) {
+                if (nums[i]+nums[i1]==target){
+                    return new int[]{i,i1};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    static Map<Integer, Integer> map = new HashMap<>();
+    public static int[] twoSum2(int[] nums, int target) {
+        map.clear();
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target-nums[i]),i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args){
+        int[] numArr = new int[5000];
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i]=i;
+        }
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < numArr.length*2; i++) {
+            twoSum1(numArr,i);
+        }
+        System.out.println("twoSum1耗时"+(System.currentTimeMillis()-startTime));
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < numArr.length*2; i++) {
+            twoSum2(numArr,i);
+        }
+        System.out.println("towSum2耗时"+(System.currentTimeMillis()-startTime));
+    }
+
+
+}
---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L2Solution.java
@@ -0,0 +1,168 @@
+package com.sky.algorithmleetcode;
+
+import java.math.BigInteger;
+
+/**
+ * @author syj
+ * @version V1.0.0
+ * @contact shiyunjie45@163.com
+ * <ul>
+ * @description 求两数之和
+ * </ul>
+ * @className L2Solution
+ * @createdTime 2023/4/18 15:33
+ */
+public class L2Solution {
+    public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+
+      public static ListNode addTwoNumbers1(ListNode l1,ListNode l2){
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(l1.val);
+        while(null!=l1.next){
+            l1 = l1.next;
+            stringBuffer.append(l1.val);
+        }
+        StringBuffer stringBuffer1 = new StringBuffer();
+        stringBuffer1.append(l2.val);
+        while(null!=l2.next){
+            l2 = l2.next;
+            stringBuffer1.append(l2.val);
+        }
+        String value = String.valueOf(new BigInteger(stringReversalRecursion(stringBuffer.toString())).add(new BigInteger(stringReversalRecursion(stringBuffer1.toString()))).toString());
+        stringBuffer.delete(0,stringBuffer.length());
+        stringBuffer1.delete(0,stringBuffer1.length());
+        ListNode resultNode = new ListNode(Integer.valueOf(value.substring(value.length()-1,value.length())));
+        value = value.substring(0,value.length()-1);
+        ListNode firstNode = resultNode;
+        while(value.length()>0){
+            resultNode.next = new ListNode(Integer.valueOf(value.substring(value.length()-1,value.length())));
+            value = value.substring(0,value.length()-1);
+            resultNode = resultNode.next;
+        }
+        return firstNode;
+      }
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        int N1 = 0;
+        int N2 = 0;
+        ListNode cur = l1;
+        while (cur != null){
+            N1++;//统计N1长度
+            cur = cur.next;
+        }
+        cur = l2;
+        while (cur != null){
+            N2++;//统计N2长度
+            cur = cur.next;
+        }
+
+        //遍历俩链表，放入数组
+        int[] a = new int[N1];
+        int[] b = new int[N2];
+        cur = l1;
+        int index = 0;
+        while (cur != null){
+            a[index++] = cur.val;//转移
+            cur = cur.next;
+        }
+        cur = l2;
+        index = 0;
+        while (cur != null){
+            b[index++] = cur.val;//转移
+            cur = cur.next;
+        }
+
+        //可以玩加法了
+        int c = 0;//进位
+        index = 0;
+        int N = N1>N2?N1+1:N2+1;//最大就是结果--多备份一个位置，万一是进位多出来了呢
+        int[] ans = new int[N];//结果
+        for (; index < N1 && index < N2; index++) {
+            //最多不会超短的长度
+            int tmp = a[index] + b[index] + c;//带进位加法
+            c = tmp/10;//把最高位拿下来11/10=1,进位，11%10模取余是1,这个是加法之后的非进位数
+            ans[index] = tmp % 10;
+        }
+        //把剩余的转移到ans中，把最高位那个进位带上
+        for (; index < N - 1; index++) {//多备份那个位置不管
+            //转移余下的数--看是谁长，加谁，a长转移a的，b长转移b的
+            int tmp = (N1 >= N2 ? a[index] : b[index]) + c;//带进位加法
+            c = tmp/10;//把最高位拿下来11/10=1,进位，11%10模取余是1,这个是加法之后的非进位数
+            ans[index] = tmp % 10;
+        }
+        //注意，可能最后还有一个c=1呢，要加上,c是0就算了
+        ans[N - 1] = c != 0 ? c : 0;
+        //然后把数组整为链表
+        ListNode head = new ListNode(ans[0]);//头
+        cur = head;
+        for (int i = 1; i < N; i++) {
+            if (i == N -1 && ans[i] == 0) break;//最后那个位置是0就算了
+            ListNode newNode = new ListNode(ans[i]);
+            cur.next = newNode;
+            cur = cur.next;
+        }
+        return head;//挺复杂的哇
+    }
+
+    public static String stringReversalRecursion(String str){
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+        return stringReversalRecursion(str.substring(1)) + str.charAt(0);
+    }
+
+    public static ListNode arrToListNode(int[] arr){
+        ListNode listNode = null;
+        ListNode firstNode = null;
+        for (int i = 0; i < arr.length; i++) {
+            if (0==i){
+                listNode = new ListNode(arr[i]);
+                firstNode = listNode;
+            }else{
+                listNode.next = new ListNode(arr[i]);
+                listNode = listNode.next;
+            }
+        }
+        return firstNode;
+    }
+
+    public static void main(String[] args){
+        int[] arr1 = {8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,0,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8};
+        int[] arr2 = {6,7,8,0,8,5,8,9,7,6,5,3,8,4,5,6,7,8,9,0,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8,8,6,5,6,8,3,5,7,8,9,1,2,3,4,5,6,7,8};
+        ListNode listNode1 = arrToListNode(arr1);
+        ListNode listNode2 = arrToListNode(arr2);
+        ListNode firstListNode1 = listNode1;
+        ListNode firstListNode2 = listNode2;
+        long startTime = System.currentTimeMillis();
+        ListNode listNode = addTwoNumbers1(firstListNode1,firstListNode2);
+        System.out.print(listNode.val);
+        while (listNode.next!=null){
+            listNode = listNode.next;
+            System.out.print(listNode.val);
+        }
+        System.out.println();
+        System.out.println("addTwoNumbers1耗时:"+(System.currentTimeMillis()-startTime));
+
+        startTime = System.currentTimeMillis();
+        listNode = addTwoNumbers2(firstListNode1,firstListNode2);
+        System.out.print(listNode.val);
+        while (listNode.next!=null){
+            listNode = listNode.next;
+            System.out.print(listNode.val);
+        }
+        System.out.println();
+        System.out.println("addTwoNumbers2耗时:"+(System.currentTimeMillis()-startTime));
+    }
+
+
+}
