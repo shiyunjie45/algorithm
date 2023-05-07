12367d4be8af48b1e9314e121dd08f0e72214e96
shiyunjie <jiejie12530@163.com>
2023-05-07 14:07:02 +0800

    调整编码

--- algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L15Solution.java
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L15Solution.java
@@ -1,12 +1,14 @@
 package com.sky.algorithmleetcode;
 
 /*
-给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三
-元组。 注意：答案中不可以包含重复的三元组。   示例： 给定数组 nums = [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [  
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三
+元组。 注意：答案中不可以包含重复的三元组。   示例： 给定数组 nums = [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [  
 [-1, 0, 1],  [-1, -1, 2] ]
 */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class L15Solution {
     public List<List<Integer>> threeSum(int[] nums) {
