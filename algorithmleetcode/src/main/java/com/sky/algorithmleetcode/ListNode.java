080a0a9e73a54d19f28ebd517fcb47307270e1c7
shiyunjie <jiejie12530@163.com>
2023-05-13 23:44:56 +0800

    将ListNode作为一个公用类

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/ListNode.java
@@ -0,0 +1,19 @@
+package com.sky.algorithmleetcode;
+
+/**
+ * @author syj
+ * @version V1.0.0
+ * @contact jiejie12530@163.com
+ * <ul>
+ * @description 链表
+ * </ul>
+ * @className ListNode
+ * @createdTime 2023/5/13 23:43
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
