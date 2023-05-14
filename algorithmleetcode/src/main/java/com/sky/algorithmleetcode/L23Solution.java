197cccf5f0e068e04fd7db064ba199c996e6d121
shiyunjie <jiejie12530@163.com>
2023-05-14 14:27:56 +0800

    23.合并K个排序链表

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L23Solution.java
@@ -0,0 +1,37 @@
+package com.sky.algorithmleetcode;
+
+/*
+合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。 示例: 输入: [   1->4->5,   1->3->4,   2->6 ] 输出:
+ 1->1->2->3->4->4->5->6
+*/
+
+ class L23Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(lists.length, Comparator.comparingInt(node -> node.val));
+
+        ListNode dummyHead = new ListNode(-1);
+        ListNode cur = dummyHead;
+
+        for (ListNode list : lists) {
+            if (list != null) {
+                minHeap.offer(list);
+            }
+        }
+
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            cur.next = node;
+            cur = cur.next;
+
+            if (node.next != null) {
+                minHeap.offer(node.next);
+            }
+        }
+
+        return dummyHead.next;
+    }
+} 
