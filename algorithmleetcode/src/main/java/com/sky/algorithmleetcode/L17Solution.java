2495481572bc9ebd97ddf03bb781b6cef559f283
shiyunjie <jiejie12530@163.com>
2023-05-19 00:01:25 +0800

    调整文件编码

--- algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L17Solution.java
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L17Solution.java
@@ -1,12 +1,11 @@
 package com.sky.algorithmleetcode;
 
 /*
-给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。  示例: 输入："2
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。  示例: 输入："2
 3" 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].  说明: 尽管上面的答案是按字典序排
 列的，但是你可以任意选择答案输出的顺序。
 */
 
-import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L23Solution.java
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L23Solution.java
@@ -1,11 +1,14 @@
 package com.sky.algorithmleetcode;
 
 /*
-合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。 示例: 输入: [   1->4->5,   1->3->4,   2->6 ] 输出:
+合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。 示例: 输入: [   1->4->5,   1->3->4,   2->6 ] 输出:
  1->1->2->3->4->4->5->6
 */
 
- class L23Solution {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class L23Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
