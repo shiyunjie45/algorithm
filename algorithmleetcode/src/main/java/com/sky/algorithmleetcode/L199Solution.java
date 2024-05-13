0d895e5bdab5ccbfb05208d0c6e1635a397f99bd
shiyunjie <jiejie12530@163.com>
2024-05-13 23:43:26 +0800

    199.二叉树的右视图

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L199Solution.java
@@ -0,0 +1,38 @@
+package com.sky.algorithmleetcode;
+
+/*
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 示例: 输入: [1,2,3,null,5,null,4] 输出: 
+[1, 3, 4] 解释:   1
+*/
+
+import java.util.*;
+
+public class L199Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return result;
+    }
+} 
