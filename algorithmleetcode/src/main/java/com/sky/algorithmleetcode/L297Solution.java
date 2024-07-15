d89605cd6db5df011ff8339216618b7f4b96707b
shiyunjie <jiejie12530@163.com>
2024-07-15 16:49:30 +0800

    297.二叉树的序列化与反序列化

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L297Solution.java
@@ -0,0 +1,51 @@
+package com.sky.algorithmleetcode;
+
+/*
+序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重
+构得到原数据。 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将
+这个字符串反序列化为原始的树结构。 示例:  你可以将以下二叉树：   1  / \  2  3   / \   4  5 序列化为 "[1,2,3,null,
+null,4,5]" 提示: 这与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用
+其他的方法解决这个问题。 说明: 不要使用类的成员 / 全局 / 静态变量来存储状态，你的序列化和反序列化算法应该是无状态的。
+*/
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class L297Solution {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "null";
+        }
+        String left = serialize(root.left);
+        String right = serialize(root.right);
+        return root.val + "," + left + "," + right;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return buildTree(queue);
+    }
+
+    private TreeNode buildTree(Queue<String> queue) {
+        String value = queue.poll();
+        if ("null".equals(value)) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(value));
+        node.left = buildTree(queue);
+        node.right = buildTree(queue);
+        return node;
+    }
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+} 
