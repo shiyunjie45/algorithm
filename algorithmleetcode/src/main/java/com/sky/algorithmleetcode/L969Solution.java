a91afcba5a650e48495b4171ceb255d9844b0bf5
shiyunjie <jiejie12530@163.com>
2026-01-07 22:25:25 +0800

    969.煎饼排序

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L969Solution.java
@@ -0,0 +1,51 @@
+package com.sky.algorithmleetcode;
+
+/*
+给定数组 A，我们可以对其进行煎饼翻转：我们选择一些正整数 k ，然后反转 A 的前 k 个元素的顺序。我们要执行零次或多次煎饼翻转（按顺序一次接一次地进行）以
+完成对数组 A 的排序。 返回能使 A 排序的煎饼翻转操作所对应的 k 值序列。任何将数组排序且翻转次数在 10 * A.length 范围内的有效答案都将被判
+断为正确。   示例 1： 输入：[3,2,4,1] 输出：[4,2,4,3] 解释： 我们执行 4 次煎饼翻转，k 值分别为 4，2，4，和 3。 初始状态 
+A = [3, 2, 4, 1] 第一次翻转后 (k=4): A = [1, 4, 2, 3] 第二次翻转后 (k=2): A = [4, 1, 2, 3] 第
+三次翻转后 (k=4): A = [3, 2, 1, 4] 第四次翻转后 (k=3): A = [1, 2, 3, 4]，此时已完成排序。  示例 2： 输入：
+[1,2,3] 输出：[] 解释： 输入已经排序，因此不需要翻转任何内容。 请注意，其他可能的答案，如[3，3]，也将被接受。    提示：  	1 	A[i]
+ 是 [1, 2, ..., A.length] 的排列
+*/
+
+ L969Solution {
+    public List<Integer> pancakeSort(int[] A) {
+        List<Integer> result = new ArrayList<>();
+        int len = A.length;
+        for (int i = len; i >= 1; i--) { // 从大到小遍历数组，每次翻转前 i 个元素，使 A[i] 最大
+            int maxIndex = findMaxIndex(A, i);
+            if (maxIndex != i - 1) { // 如果 A[i] 不是最后一个元素，则需要先翻转前 maxIndex+1 个元素，再翻转前 i 个元素，使 A[i] 移动到最后位置
+                reverse(A, maxIndex);
+                result.add(maxIndex + 1);
+                reverse(A, i - 1);
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    // 在数组前 n 个元素中找到最大值的下标
+    private int findMaxIndex(int[] A, int n) {
+        int maxIndex = 0;
+        for (int i = 1; i < n; i++) {
+            if (A[i] > A[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    // 反转数组前 n 个元素的顺序
+    private void reverse(int[] A, int n) {
+        int left = 0, right = n;
+        while (left < right) {
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] = temp;
+            left++;
+            right--;
+        }
+    }
+} 
