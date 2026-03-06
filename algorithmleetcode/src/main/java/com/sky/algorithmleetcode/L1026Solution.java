5eed200994c0c8a723b4641f041da740ae155505
shiyunjie <jiejie12530@163.com>
2026-03-06 13:58:50 +0800

    1026.节点与其祖先之间的最大差值

---
+++ algorithmleetcode/src/main/java/com/sky/algorithmleetcode/L1026Solution.java
@@ -0,0 +1,58 @@
+package com.sky.algorithmleetcode;
+
+/*
+给定二叉树的根节点 root，找出存在于不同节点 A 和 B 之间的最大值 V，其中 V = |A.val - B.val|，且 A 是 B 的祖先。 （如果 
+A 的任何子节点之一为 B，或者 A 的任何子节点是 B 的祖先，那么我们认为 A 是 B 的祖先）   示例：  输入：[8,3,10,1,6,null,14
+,null,null,4,7,13] 输出：7 解释： 我们有大量的节点与其祖先的差值，其中一些如下： |8 - 3| = 5 |3 - 7| = 4 |8 -
+ 1| = 7 |10 - 13| = 3 在所有可能的差值中，最大值 7 由 |8 - 1| = 7 得出。    提示：  	树中的节点数在 2 到 500
+0 之间。 	每个节点的值介于 0 到 100000 之间。
+*/
+
+ class L1026Solution {
+    int res = Integer.MIN_VALUE;
+
+    public int maxAncestorDiff(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root, root.val, root.val);
+        return res;
+    }
+
+    private void dfs(TreeNode node, int maxVal, int minVal) {
+        if (node == null) {
+            res = Math.max(res, maxVal - minVal);
+            return;
+        }
+        maxVal = Math.max(maxVal, node.val);
+        minVal = Math.min(minVal, node.val);
+        dfs(node.left, maxVal, minVal);
+        dfs(node.right, maxVal, minVal);
+    }
+
+    public static void main(String[] args) {
+        // 测试用例
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(10);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(6);
+        root.left.right.left = new TreeNode(4);
+        root.left.right.right = new TreeNode(7);
+        root.right.right = new TreeNode(14);
+        root.right.right.left = new TreeNode(13);
+
+        L1026Solution solution = new L1026Solution();
+        System.out.println(solution.maxAncestorDiff(root)); // 输出7
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+} 
